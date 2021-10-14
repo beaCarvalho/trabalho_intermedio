c8b5566e702e047ecdcbd59bf244777dfc06e3c7
RodrigoCruz20 <76444512+RodrigoCruz20@users.noreply.github.com>
2021-10-14 21:06:06 +0100

    Delete projectcollapse.js

--- projectcollapse.js
+++
@@ -1,9 +0,0 @@
-$('#iconchange1').click(function(){
-$(this).find('i').toggleClass('glyphicon glyphicon-chevron-down').toggleClass('glyphicon glyphicon-chevron-up');
-$("div.table1").find("div.hiddenRow").toggleClass('hiddenRow2');
-});
-
-$('#iconchange2').click(function(){
-$(this).find('i').toggleClass('glyphicon glyphicon-chevron-down').toggleClass('glyphicon glyphicon-chevron-up');
-$("div.table2").find("div.hiddenRow").toggleClass('hiddenRow2');
-});
